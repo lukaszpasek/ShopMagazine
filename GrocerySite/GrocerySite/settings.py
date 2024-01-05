1760164cf54fd6d36be3c4e30eea5ada5815190f
lukaszpasek <lukasz.pasek7@wp.pl>
2024-01-05 21:36:52 +0100

    setup project

--- GrocerySite/GrocerySite/settings.py
+++ GrocerySite/GrocerySite/settings.py
@@ -80,8 +80,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'groceryapp',
-        'USERNAME': 'root',
-        'PASSWORD': '',
+        'USERNAME': 'lpas',
+        'PASSWORD': 'root',
         'HOST': 'localhost',
         'PORT': '5432',
     }
--- GrocerySite/manage.py
+++ GrocerySite/manage.py
@@ -7,6 +7,7 @@ import sys
 def main():
     """Run administrative tasks."""
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GrocerySite.settings')
+    os.environ.setdefault("DATABASE_URL", "postgres://root:root@localhost:5432/groceryapp")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
